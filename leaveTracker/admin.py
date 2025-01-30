0ef512dced4ef8c4590967e50434b70e40429516
muansuiu <mnezami173053@bscse.uiu.ac.bd>
2025-01-30 23:26:34 +0600

    # Feature: - included LeaveRequest model

--- leaveTracker/admin.py
+++ leaveTracker/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Employees, Team
+from .models import Employees, Team, LeaveRequest
 
 # Register your models here.
 
 admin.site.register(Employees)
 admin.site.register(Team)
+admin.site.register(LeaveRequest)
