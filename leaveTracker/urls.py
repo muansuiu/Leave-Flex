ffd85924014c25e1c51c631386f91b11432992a3
muansuiu <mnezami173053@bscse.uiu.ac.bd>
2025-02-13 00:39:17 +0600

    # Feature: - integrated bootstrap 5 for responsive design - login functionality implemented

--- leaveTracker/urls.py
+++ leaveTracker/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import EmployeeLoginView, SignUpView, get_roles
+from .views import EmployeeLoginView, SignUpView, get_roles, HomeView
 
 urlpatterns = [
     path('sign-up', SignUpView.as_view(), name='sign-up'),
     path('login/', EmployeeLoginView.as_view(), name='login'),
     path('get_roles/', get_roles, name='get_roles'),
+    path('home', HomeView.as_view(), name='home')
 ]
--- leaveTracker/views.py
+++ leaveTracker/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib import messages
+from django.views.generic.base import TemplateView
 
 from leaveTracker import roles_filename
 from leaveTracker.utils.utils import Utils
@@ -15,7 +17,7 @@ utils = Utils()
 TEAM_ROLE_CHOICES = utils.read_json(roles_filename)
 
 class SignUpView(SuccessMessageMixin, CreateView):
-    template_name = 'sign-up.html'
+    template_name = 'registration/sign-up.html'
     success_url = reverse_lazy('login')
     form_class = UserRegisterForm
     success_message = "Profile Created Successfully"
@@ -29,9 +31,20 @@ class SignUpView(SuccessMessageMixin, CreateView):
         return super().form_valid(form)
 
 class EmployeeLoginView(LoginView):
-    template_name = 'login.html'
+    template_name = 'registration/login.html'
     success_url = reverse_lazy('home')
 
+    def get_success_url(self):
+        messages.success(self.request, 'Welcome!')
+        return self.success_url
+    
+    def form_invalid(self, form):
+        messages.error(self.request,'Invalid username or password')
+        return super().form_invalid(form)
+    
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    
 # New API endpoint for fetching roles dynamically
 def get_roles(request):
     team_name = request.GET.get("team", "")
